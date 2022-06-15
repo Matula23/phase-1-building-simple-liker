409e20ace77b47c1f13adc229da596c1787cd9e7
Maciej Matula <maciek.matula@gmail.com>
2022-06-15 08:19:50 -0500

    commit tree 68de46b3dc890c4c39d4883818572896d97acaaf

--- main.js
+++ main.js
@@ -2,10 +2,49 @@
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
+
 // Your JavaScript code goes here!
+document.addEventListener("DOMContentLoaded", () => {
+  errorHide()
+  clickListener()
+})
 
+function clickListener(){
+  document.addEventListener("click", (event) => {
+    const glyphElement = event.target
+    if(glyphElement.classList.value === "like-glyph" && glyphElement.innerText === EMPTY_HEART){
+      mimicServerCall()
+        .then(resp => {
+          glyphElement.textContent = FULL_HEART
+          glyphElement.classList.add("activated-heart")
+        })
+        .catch(error => {
+          errorShow()
+          setTimeout(errorHide, 3000);
+        })
+    } else if (glyphElement.classList.contains("like-glyph") && glyphElement.innerText === FULL_HEART){
+      glyphElement.textContent = EMPTY_HEART
+      glyphElement.classList.remove("activated-heart")
+    }
+  })
+}
 
+function FullHeartClick(){
+  document.addEventListener("click", (event) => {
+    // if(event.target.classList.value === "like-glyph" && event.target.value)
+    console.log(event.target.value)
+  })
+}
 
+function errorHide(){
+  const errorModal = document.querySelector("#modal")
+  errorModal.className = "hidden"
+}
+
+function errorShow(){
+  const errorModal = document.querySelector("#modal")
+  errorModal.className = ""
+}
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
